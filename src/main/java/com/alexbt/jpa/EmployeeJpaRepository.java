4bdd63670955a895f2de08172320eb41c11d9bd6
Alex Belisle Turcot <alex.belisleturcot@gmail.com>
2016-07-23 00:15:41 -0400

    org. imports

--- src/main/java/com/alexbt/jpa/EmployeeJpaRepository.java
+++ src/main/java/com/alexbt/jpa/EmployeeJpaRepository.java
@@ -1,12 +1,6 @@
 package com.alexbt.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-//@RepositoryRestResource(path="departements/{department_id}/employees", collectionResourceRel = "employees")
-//@RestResource(path="departements/{department_id}/employees", rel = "employees")
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Long> {
 }
